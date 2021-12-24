ee41df9db0b53252701765e7def9603e055f6153
GordonWu <ghostsas0110@gmail.com>
2021-12-24 21:13:10 +0800

    init

---
+++ app/src/main/java/gordon/lab/github_user_mvc/MainActivity.kt
@@ -0,0 +1,11 @@
+package gordon.lab.github_user_mvc
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
